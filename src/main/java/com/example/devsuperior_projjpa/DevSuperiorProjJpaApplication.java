407ec4c96a9813871c6d080670482fee30790070
lipollis <aline.pollis@hotmail.com>
2022-09-28 13:45:23 -0300

    first commit

---
+++ src/main/java/com/example/devsuperior_projjpa/DevSuperiorProjJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.devsuperior_projjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSuperiorProjJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevSuperiorProjJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devsuperior_projjpa/DevSuperiorProjJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devsuperior_projjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSuperiorProjJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
